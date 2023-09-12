8943a4e2e7cd42f3446b02b967a3f0e1736bbc2e
FelipeCostaOFM <141030275+FelipeCostaOFM@users.noreply.github.com>
2023-09-12 19:06:04 -0300

    Revert "tela de cadastro e mudanças no estilo"
    This reverts commit fe6aabcef7bffd216d0b586ccdaa9b61addffbeb.

--- frontend/src/Components/Views/Header-Footer/index.jsx
+++ frontend/src/Components/Views/Header-Footer/index.jsx
@@ -10,7 +10,6 @@ function HeaderPage() {
                         <li><a href="/">Home</a></li>
                         <li><a href="/contato">Eventos</a></li>
                         <li><a href="/contato">Crie seu Evento</a></li>
-                        <li><a href="/SignIn">Login</a></li>
                         <li><a href="/sobre">Sobre nós</a></li>
                     </ul>
                 </nav>
